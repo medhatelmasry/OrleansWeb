8e58bf672c3e0006fc156a0316d2d7387228ce02
Medhat Elmasry <medhat@elmasry.ca>
2023-02-15 15:30:05 -0800

    Update Index.cshtml.cs

--- RazorPagesClient/Pages/Index.cshtml.cs
+++ RazorPagesClient/Pages/Index.cshtml.cs
@@ -1,25 +1,44 @@
-﻿using GrainInterfaces;
-using System.Xml.Linq;
+using GrainInterfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Orleans;
 
-namespace RazorPagesClient.Pages;
+namespace RazorClient.Pages;
 
-public class IndexModel : PageModel
-{
+public class IndexModel : PageModel {
     private readonly ILogger<IndexModel> _logger;
     private readonly IClusterClient _clusterClient;
+    public string Message { get; set; } = "";
 
-    public IndexModel(ILogger<IndexModel> logger,
-        IClusterClient clusterClient)
-    {
+    [BindProperty]
+    public string? Key { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IClusterClient clusterClient) {
         _logger = logger;
         _clusterClient = clusterClient;
     }
 
-    public async Task OnGet()
+    public void OnGet() { }
+
+    public async Task OnPostDecrement(int count)
     {
-        var result = await _clusterClient.GetGrain<IPersonGrain>("Fred").SayHelloAsync();
-        ViewData["response"] = result;
+        var counterGrain = _clusterClient.GetGrain<ICounterGrain>(Key);
+        Message = await counterGrain.Decrement(count);
+    }
+
+    public async Task OnPostIncrement(int count) {
+        var counterGrain = _clusterClient.GetGrain<ICounterGrain>(Key);
+        Message = await counterGrain.Increment(count);
+    }
+    public async Task OnPostCount() {
+        var counterGrain = _clusterClient.GetGrain<ICounterGrain>(Key);
+        var currentCount = await counterGrain.GetCount();
+
+        Message = $"Current value of count for '{Key}' is {currentCount}";
+    }
+
+    public void OnPostKey(string key) {
+        Key = key;
+        Message = $"Current value of key is '{Key}'";
     }
 }
